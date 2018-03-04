9bd792aec74531f3cdd635072296c1d2e227ae94
kim366 <kim.schmider@hotmail.com>
2018-03-04 14:56:33 +0100

    Add SFML code

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,20 @@
+#include <SFML/Graphics.hpp>
+
 int main()
 {
-	
+	sf::RenderWindow window({900, 900}, "SFML");
+
+	while (true)
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed || event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
+				return 0;
+		}
+
+		window.clear();
+		// window.draw();
+		window.display();
+	}
 }
