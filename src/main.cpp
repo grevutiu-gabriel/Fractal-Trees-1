b3d519ea0ebbfc4e3e8aa3749c31f583ac5e9922
kim366 <kim.schmider@hotmail.com>
2018-03-05 16:29:32 +0100

    Begin update method and add variable length branches

--- src/main.cpp
+++ src/main.cpp
@@ -35,8 +35,13 @@ struct Branch : public sf::Drawable
 			, order < 8 ? map(static_cast<float>(order), {0, 8}, {10, 1}) : 12
 			, {66, 35, 24}}
 		, angle{angle_ + parent->angle}
+		, last_angle{0}
+		, elapsed_frames{0}
 	{
 		line.setRounded(true);
+		std::mt19937 rng{std::random_device{}()};
+		std::uniform_int_distribution<int> rand_length_mult{50, 150};
+		line.setPoint(1, line.getPoint(0) + (line.getPoint(1) - line.getPoint(0)) * (static_cast<float>(rand_length_mult(rng)) / 100));
 	};
 
 	Branch(sf::Vector2f position_)
@@ -44,8 +49,30 @@ struct Branch : public sf::Drawable
 		, order{0}
 		, line{{position_}, {position_ - sf::Vector2f{0, 70}}, map(static_cast<float>(order), {0, 8}, {10, 1}), {66, 35, 24}}
 		, angle{0}
+		, last_angle{0}
+		, elapsed_frames{0}
 	{
 		line.setRounded(false);
+	};
+
+	void update(float dt_)
+	{
+		if (parent == nullptr)
+		{
+			auto vec{line.getPoint(1) - line.getPoint(0)};
+			last_angle = angle;
+			angle = std::sin(static_cast<float>(elapsed_frames) * dt_ * 3.14159f) * 2.f;
+			if (angle == 0)
+				elapsed_frames = 0;
+			std::cout << angle << '\n';
+			line.setPoint(1, line.getPoint(0) + sf::Transform{}.rotate(angle - last_angle).transformPoint(vec));
+		}
+		else
+		{
+			line.setPoint(0, parent->line.getPoint(0));
+		}
+
+		++elapsed_frames;
 	};
 
 	void draw(sf::RenderTarget& target_, sf::RenderStates states_) const override
@@ -59,7 +86,7 @@ struct Branch : public sf::Drawable
 			rect.setPoint(1, {16, 0});
 			rect.setPoint(2, {22, 10});
 			rect.setPoint(3, {6, 10});
-			rect.setFillColor({186, 40, 67, 120});
+			rect.setFillColor({134, 255, 74, 80});
 			rect.setOrigin({1, 1});
 			rect.rotate(angle - 180);
 			rect.setPosition(line.getPoint(0));
@@ -70,8 +97,9 @@ struct Branch : public sf::Drawable
 	const Branch* parent;
 	int order;
 	sw::Line line;
-	float angle;
+	float angle, last_angle;
 	float width;
+	int elapsed_frames;
 };
 
 class Tree
@@ -115,6 +143,14 @@ private:
 int main()
 {
 	sf::RenderWindow window({1900, 1000}, "SFML", sf::Style::Default, sf::ContextSettings{0, 0, 4});
+	sf::VertexArray background{sf::Quads, 4};
+	background[0].position = {0, 0};
+	background[1].position = {1900, 0};
+	background[2].position = {1900, 1000};
+	background[3].position = {0, 1000};
+
+	background[0].color = background[1].color = {235, 129, 61};
+	background[2].color = background[3].color = {235, 63, 103};
 
 	std::vector<Tree> trees;
 
@@ -122,16 +158,33 @@ int main()
 		for (float j{0}; j < 4; ++j)
 			trees.push_back({{450.f * j + 250.f, 400.f * (i + 1)}});
 
+	sf::Clock clock;
+	float lag{0.f};
+	const float delta_time{0.0069f};
 	while (true)
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
+		sf::Time elapsed = clock.restart();
+		lag += elapsed.asSeconds();
+
+		while (lag >= delta_time)
 		{
-			if (event.type == sf::Event::Closed || (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape))
-				return 0;
+			sf::Event event;
+			while (window.pollEvent(event))
+			{
+				if (event.type == sf::Event::Closed || (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape))
+					return 0;
+			}
+
+			for (auto& tree : trees)
+				for (auto& branch : tree)
+					branch.update(delta_time);
+
+			lag -= delta_time;	
 		}
 
+
 		window.clear(sf::Color::White);
+		window.draw(background);
 		for (auto& tree : trees)
 			for (const auto& branch : tree)
 				window.draw(branch);
